463e5f1598c54befe5324a21439b3a82f4ffb1b5
Gavin Douglas <gavinmdouglas@gmail.com>
2022-08-01 17:09:23 -0400

    test files

--- testing/contrib_to_list_test.R
+++ testing/contrib_to_list_test.R
@@ -1,38 +1,53 @@
 rm(list = ls(all.names = TRUE))
 
 library("collapse")
+library("data.table")
 
 samp_colname <- "sample"
 func_colname <- "function."
 abun_colname <- "taxon_abun"
 taxon_colname <- "taxon"
 
-test_file <- read.table("/Users/gavin/Google Drive/My Drive/postdoc/FuncDiv/isabel_project/data/testfiles/test_contrib_file/hmp_picrust2_v2.4.0-b_path_abun_contrib.tsv.gz",
+contrib_tab <- read.table("/home/gdouglas/tmp/hmp_picrust2_v2.4.0-b_path_abun_contrib.tsv.gz",
                         sep = "\t", header = TRUE, stringsAsFactors = FALSE, quote = "", comment.char = "")
 
-test_file <- test_file[, c(samp_colname, func_colname, taxon_colname, abun_colname)]
+contrib_tab <- contrib_tab[, c(samp_colname, func_colname, taxon_colname, abun_colname)]
 
-test_file <- test_file[c(1, 2, 3, 100, 660857, 190002, 190001, 660858, 2000, 190003), ]
+contrib_tab_split <- collapse::rsplit(x = contrib_tab, by = contrib_tab$function.)
 
-test_file <- test_file[collapse::radixorder(test_file[, samp_colname], test_file[, func_colname]), ]
+tmp_in <- ontrib_tab_split[[1]]
 
-group_starts <- attr(radixorder(test_file[, samp_colname], test_file[, func_colname], starts = TRUE, sort = FALSE), "starts")
+tmp <- data.table::dcast.data.table(data = data.table(contrib_tab_split[[1]]),
+                                    formula =  contrib_tab_split[[1]]$taxon ~ contrib_tab_split[[1]]$sample,
+                                    value.var = 'taxon_abun')
 
-sample_set <- test_file[group_starts, samp_colname]
-func_set <- test_file[group_starts, func_colname]
+tmp[is.na(tmp)] <- 0
+
+rownames(tmp) <- tmp$contrib_tab_split
+
+tmp <- tmp[, -1]
+
+tmp <- sweep(x = tmp, MARGIN = 2, STATS = colSums(tmp), '/') * 100
+
+contrib_tab <- contrib_tab[collapse::radixorder(contrib_tab[, samp_colname], contrib_tab[, func_colname]), ]
+
+group_starts <- attr(radixorder(contrib_tab[, samp_colname], contrib_tab[, func_colname], starts = TRUE, sort = FALSE), "starts")
+
+sample_set <- contrib_tab[group_starts, samp_colname]
+func_set <- contrib_tab[group_starts, func_colname]
 
 abun_values <- list()
 num_groups <- length(group_starts)
 for (i in 1:(num_groups - 1)) {
-  abun_values[[i]] <- test_file[group_starts[i]:(group_starts[i + 1] - 1), abun_colname]
+  abun_values[[i]] <- contrib_tab[group_starts[i]:(group_starts[i + 1] - 1), abun_colname]
 }
-abun_values[[i]] <- test_file[group_starts[num_groups]:nrow(test_file), abun_colname]
+abun_values[[num_groups]] <- contrib_tab[group_starts[num_groups]:nrow(contrib_tab), abun_colname]
 
 
 taxa_labels <- list()
 num_groups <- length(group_starts)
 for (i in 1:(num_groups - 1)) {
-  taxa_labels[[i]] <- test_file[group_starts[i]:(group_starts[i + 1] - 1), taxon_colname]
+  taxa_labels[[i]] <- contrib_tab[group_starts[i]:(group_starts[i + 1] - 1), taxon_colname]
 }
-taxa_labels[[i]] <- test_file[group_starts[num_groups]:nrow(test_file), taxon_colname]
+taxa_labels[[i]] <- contrib_tab[group_starts[num_groups]:nrow(contrib_tab), taxon_colname]
 
---
+++ testing/conversion_tests.R
@@ -0,0 +1,86 @@
+rm(list = ls(all.names = TRUE))
+
+library("collapse")
+library("data.table")
+
+samp_colname <- "sample"
+func_colname <- "function."
+abun_colname <- "taxon_abun"
+taxon_colname <- "taxon"
+copy.num_colname <- "genome_function_count"
+
+contrib_tab <- read.table("/home/gdouglas/tmp/hmp_picrust2_v2.4.0-b_path_abun_contrib.tsv.gz",
+                        sep = "\t", header = TRUE, stringsAsFactors = FALSE, quote = "", comment.char = "")
+
+
+source("/home/gdouglas/scripts/FuncDiv/R/utils.R")
+
+multi_tab <- contrib_to_multitab(contrib_tab = contrib_tab,
+                                 samp_colname = "sample",
+                                 func_colname = "function.",
+                                 abun_colname = "taxon_abun",
+                                 taxon_colname = "taxon",
+                                 copy.num_colname = "genome_function_count")
+
+contrib_tab_remade <- multitab_to_contrib(func_tab = multi_tab$function_copy_num,
+                                          abun_tab = multi_tab$taxon_abun,
+                                          ncores = 20,
+                                          samp_colname = "sample",
+                                          func_colname = "function.",
+                                          abun_colname = "taxon_abun",
+                                          taxon_colname = "taxon",
+                                          copy.num_colname = "genome_function_count")
+
+contrib_tab_orig <- contrib_tab[, colnames(contrib_tab_remade)]
+
+contrib_tab_orig <- contrib_tab_orig[order(contrib_tab_orig$sample, contrib_tab_orig$function., contrib_tab_orig$taxon), ]
+contrib_tab_remade <- contrib_tab_remade[order(contrib_tab_remade$sample, contrib_tab_remade$function., contrib_tab_remade$taxon), ]
+
+rownames(contrib_tab_orig) <- NULL
+rownames(contrib_tab_remade) <- NULL
+
+all.equal(contrib_tab_remade, contrib_tab_orig)
+
+
+
+
+
+
+func_tab_orig <- read.table("/home/gdouglas/scripts/FuncDiv/example_files/func_input.tsv.gz",
+                            header = TRUE, sep = "\t", row.names = 1)
+
+taxa_tab_orig <- read.table("/home/gdouglas/scripts/FuncDiv/example_files/taxa_input.tsv.gz",
+                            header = TRUE, sep = "\t", row.names = 1)
+
+contrib_tab_out <- multitab_to_contrib(func_tab = func_tab_orig,
+                                          abun_tab = taxa_tab_orig,
+                                          ncores = 20,
+                                          samp_colname = "sample",
+                                          func_colname = "function.",
+                                          abun_colname = "taxon_abun",
+                                          taxon_colname = "taxon",
+                                          copy.num_colname = "genome_function_count")
+
+
+subsetted_tables <- subset_func_and_abun_tables(func_table = func_tab_orig,
+                                                abun_table = taxa_tab_orig)
+func_tab_orig_subset <- subsetted_tables$func
+taxa_tab_orig_subset <- subsetted_tables$abun
+
+rm(subsetted_tables)
+
+mutlitab_remade <- contrib_to_multitab(contrib_tab = contrib_tab_out, 
+                                       samp_colname = "sample",
+                                       func_colname = "function.",
+                                       abun_colname = "taxon_abun",
+                                       taxon_colname = "taxon",
+                                       copy.num_colname = "genome_function_count")
+
+
+taxon_abun_remade <- mutlitab_remade$taxon_abun[rownames(taxa_tab_orig_subset), colnames(taxa_tab_orig_subset)]
+func_tab_remade <- mutlitab_remade$function_copy_num[rownames(func_tab_orig_subset), colnames(func_tab_orig_subset)]
+
+all.equal(taxon_abun_remade, taxa_tab_orig_subset)
+all.equal(func_tab_remade, func_tab_orig_subset)
+
+
